3dca0d4dfb69175b75b671cb0c3cc1ca64e40acc
Caio <me@cafcaio.com>
2022-11-12 09:48:08 -0300

    Apresentação dos eventos melhorada com grid

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,21 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  isCollapsed: boolean = false;
+  private _filtroLista: string = "";
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  public filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((x: any) => x.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    || x.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 )
+  }
 
   constructor(private http: HttpClient) {
 
   }
 
+
   ngOnInit(): void {
     this.getEventos();
+
   }
 
   public getEventos(): void {
-    this.eventos = this.http.get("https://localhost:3000/api/evento").subscribe({
-      next: (response) => this.eventos = response,
+    this.http.get("https://localhost:3000/api/evento").subscribe({
+      next: (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error: (er) => console.log(er)
     });
+
+
+
   }
 
 }
